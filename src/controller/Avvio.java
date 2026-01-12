486659fcb16011bc9b268a0ec332b5e5ce7853e1
Your Name <you@example.com>
2026-01-12 10:35:58 +0100

    Abbiamo aggiunto i sopravissuti

--- src/controller/Avvio.java
+++ src/controller/Avvio.java
@@ -4,6 +4,8 @@ public class Avvio {
 
 	public static void main(String[] args) {
 		System.out.println("toto sopravissuti");
+		System.out.println("Il buon Mattia");
+		System.out.println("La  mistica Francesca");
 	}
 
 }
